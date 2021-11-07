4cbb6ea1a27b10fb9e4c36b4508b8d880025995c
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2021-11-07 18:01:01 +0200

    Added main functionality in the Application class

--- src/main/java/com/example/accessingdatajpa/AccessingDataJpaApplication.java
+++ src/main/java/com/example/accessingdatajpa/AccessingDataJpaApplication.java
@@ -1,13 +1,58 @@
 package com.example.accessingdatajpa;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AccessingDataJpaApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(AccessingDataJpaApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccessingDataJpaApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner foo() {
+		return args -> log.info("Hello World");
+	}
+
+	@Bean
+	public CommandLineRunner demo(CustomerRepository repository) {
+		return (args) -> {
+			// save a few customers
+			repository.save(new Customer("Jack", "Bauer"));
+			repository.save(new Customer("Chloe", "o'Brian"));
+			repository.save(new Customer("Kim", "Bauer"));
+			repository.save(new Customer("David", "Palmer"));
+			repository.save(new Customer("Michelle", "Dessler"));
+
+			// fetch all customers
+			log.info("Customers found with findAll():");
+			log.info("-------------------------------");
+			for (Customer customer : repository.findAll()) {
+				log.info(customer.toString());
+			}
+			log.info("");
+
+			// fetch an individual customer by id
+			Customer customer = repository.findById(1L);
+			log.info("Customer found with findById(1L):");
+			log.info(customer.toString());
+			log.info("");
+
+			// fetch customers by last name
+			log.info("All Bauers:");
+			for (Customer c:
+				 repository.findByLastName("Bauer")) {
+				log.info(c.toString());
+			}
+			log.info("-----------");
+			log.info("");
+		};
+	}
 }
